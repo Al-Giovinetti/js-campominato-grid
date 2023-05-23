9402306a052ae8e072d1fb91be54f4ca8450e23d
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-23 18:52:43 +0200

    Sistemato problema una sola griglia

--- js/myscript.js
+++ js/myscript.js
@@ -4,12 +4,11 @@ let mainElement = document.querySelector("main")
 // Chiudo tutto in un event listened 
 const btnPlay = document.querySelector("button");
 
-const clear=""
-
 btnPlay.addEventListener("click",function(){
     let bigGrid = getNewDivElement();
     bigGrid.classList.add("big-grid");
 
+    mainElement.innerHTML=""
     mainElement.append(bigGrid);
 
     for(let i=0; i<100 ; i++){
@@ -26,13 +25,14 @@ btnPlay.addEventListener("click",function(){
         bigGrid.appendChild(cell)
         cell.appendChild(numberCell)
     }
-
-
 })
 
 
 // FUNCTION //
-
+/**Give a div in memory, after must be append
+ * 
+ * @returns New div element
+ */
 function getNewDivElement(){
     const NewElement = document.createElement("div");
     return NewElement
