246c388daceff496a67563d52d3d1784bd33470f
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-23 17:11:50 +0200

    Griglia di base

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,22 @@
+
+let mainElement = document.querySelector("main")
+
 // Chiudo tutto in un event listened 
-const btnPlay = document.querySelector("button")
+const btnPlay = document.querySelector("button");
+
+const clear=""
 
 btnPlay.addEventListener("click",function(){
-    
+    let bigDivElement = getNewDivElement();
+    bigDivElement.classList.add("appear");
+
+    mainElement.append(bigDivElement);
 })
 
 
+// FUNCTION //
+
+function getNewDivElement(){
+    const NewElement = document.createElement("div");
+    return NewElement
+}
